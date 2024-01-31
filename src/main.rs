42ed3df782afdff5f9234e5cee3046bf072a0064
Karan Jadhav <karan.jadhavhkr@gmail.com>
2024-01-31 11:59:35 +0530

    Add tracing and logging to the application

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,16 @@ use rediserve::models::ApiError;
 use rediserve::routes::redis_routes::redis_routes;
 use rediserve::{config::AppConfig, state::AppState};
 use std::sync::Arc;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
     let config = AppConfig::new();
 
     let app_state = Arc::new(AppState::new(config.redis_url));
@@ -17,16 +24,18 @@ async fn main() {
         .route("/", get(|| async { "Hello, World!" }))
         .route("/redis", get(set_redis_key))
         .merge(redis_routes())
-        .layer(Extension(app_state));
+        .layer(Extension(app_state))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
 
     let addr = format!("0.0.0.0:{}", config.server_port);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
-    println!(
-        "Server running on http://{}",
-        listener.local_addr().unwrap()
-    );
+    println!("Server running on http://{}", addr);
 
     axum::serve(listener, app).await.unwrap();
 }
